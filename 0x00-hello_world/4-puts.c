c023d21ac330938a050207cae4f2ee40f14d97ce
lydiauzoma <lydiauzoma@yahoo.com>
2022-06-30 15:30:10 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+*main - Put out a string
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-puts( "\"programming is like building a multilingual puzzle");	return(0);
-
-} 
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
