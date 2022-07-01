428144790e1ece10a0d9059c8db387c700b3e01b
lydiauzoma <lydiauzoma@yahoo.com>
2022-06-30 19:20:49 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -8,7 +6,6 @@
  */
 int main(void)
 {
-int s = strlen("and that piece of art is useful-Dora korpar, 2015-10-19\n"s);
-write(2, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"s);
+write(2, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"59);
 return (1);
 }
