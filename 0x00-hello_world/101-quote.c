aec544ce47b35d5cbb46088027bd06a85bf09959
lydiauzoma <lydiauzoma@yahoo.com>
2022-06-30 18:38:21 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-int s = strlen("and that piece of art is useful-Dora korpar, 2015-10-19\n");
+int s = strlen("and that piece of art is useful-Dora korpar, 2015-10-19\n"s);
 write(2, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"s);
 return (1);
 }
