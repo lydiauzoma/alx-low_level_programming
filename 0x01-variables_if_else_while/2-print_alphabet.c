f80579a3cfd96c6c2d8e8d12635c4b3eecde0cb0
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-01 16:14:51 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Print the alphabet in lowercase 
- * Return: Always 0 (sucess)
+ * main - Print the alphabet in lowercase
+ * return - Always 0 (success)
  */
 int main(void)
 {
