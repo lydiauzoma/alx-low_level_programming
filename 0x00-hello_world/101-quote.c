ba66afea9b6092001fadcbeff1f7be170f5d5c1e
lydiauzoma <lydiauzoma@yahoo.com>
2022-06-30 17:44:08 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n"sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
-reture (1);
+write(1, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"59);
+return (1);
 }
