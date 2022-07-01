534052262fdde944bb7506603888d2bb506bb377
lydiauzoma <lydiauzoma@yahoo.com>
2022-06-30 19:33:15 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: 1
+ * Return: 0
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"59);
+write(1, "and that piece of art is useful\"- Dora korpar,2015-10-19\n"59);
 return (1);
 }
