55431c53c613365ba57eca3fe7c379ea03fa17a5
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-01 14:43:25 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10)
+return (0);
+}
