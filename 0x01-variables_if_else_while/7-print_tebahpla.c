2fc77cadb5e04054796796f96232c2af5eb9db42
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-01 17:37:11 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+char l;
+for (l = 'z'; l >= 'a'; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
