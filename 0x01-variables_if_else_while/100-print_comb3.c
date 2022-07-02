ee96ca9426afb64814b8a104ac785dbbf23f57bf
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-01 20:35:03 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <studio.h>
+/**
+ * main - program entry point 
+ * Return 0 on success .error code otherwise
+ */
+int main(void)
+{
+int n = 0;
+int i;
+i=n
+while (n <= 99)
+{
+i= n;
+while (i < 99>
+{
+if (i/10 % 10 > i % 10)
+break;
+i/=10;
+{
+if (i == 0)
+{
+if ((n % 11) !=0 && n != 11)
+{
+putchar (',')
+putchar('')
+}
+}
+break;
+}
+}
+putchar ('\n')
+return (0);
+}
