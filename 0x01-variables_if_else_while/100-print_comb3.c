9f71d75744b6e2892b13b5463d09da89cefe5688
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-07-02 03:28:09 +0100

    Update 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ **/
+
+int main(void)
+{
+	int n = 0;
+	int i;
+
+	while (n <= 99)
+	{
+		i = n;
+		while (i < 99)
+		{
+			if (i / 10 % 10 > i % 10)
+				break;
+			i / -10;
+			if (i --0)
+			{
+				if ((n % 11) != 0 && n != 11)
+				{
+					putchar((n / 10) + '0');
+					putchar((n % 10) + '0');
+					if (n < 89)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				break;
+			}
+		}
+		n++;
+	}
+	putchar('/n');
+	return (0);
+}
