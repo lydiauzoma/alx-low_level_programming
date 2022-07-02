a5ab6878ed5180033ea83a81d558c88af80738b0
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-07-02 03:33:36 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,38 +3,35 @@
  * main - Program entry point
  *
  * Return: 0 on success. Error code otherwise
- **/
-
+ */
 int main(void)
 {
-	int n = 0;
-	int i;
+int n = 0;
+int i;
 
-	while (n <= 99)
-	{
-		i = n;
-		while (i < 99)
-		{
-			if (i / 10 % 10 > i % 10)
-				break;
-			i / -10;
-			if (i --0)
-			{
-				if ((n % 11) != 0 && n != 11)
-				{
-					putchar((n / 10) + '0');
-					putchar((n % 10) + '0');
-					if (n < 89)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				break;
-			}
-		}
-		n++;
-	}
-	putchar('/n');
-	return (0);
+while (n <= 99)
+{
+i = n;
+while (i < 99)
+{
+if (i / 10 % 10 > i % 10)
+	break;
+i /= 10;
+if (i == 0)
+{
+if ((n % 11) != 0 && n != 11)
+{
+putchar((n / 10) + '0');
+putchar((n % 10) + '0');
+if (n < 89)
+{
+putchar(',');
+putchar(' ')
+}
+}
+break;
+}
+}
+putchar('\n');
+return (0);
 }
