ba252a0310d31abbe0ba1049d3f1cc31475e6771
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-01 18:39:24 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of single digit
+ * Reture :Always 0 .
+ */
+int mai(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar((num % 10) + '0')
+if (num == 9)
+continue;
+putchar(',')
+putchar(',')
+}
+return (0)
+
