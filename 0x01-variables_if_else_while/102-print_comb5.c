737e4eb449666293330feae03a32adde1ff211e2
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-03 14:36:03 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,39 +6,22 @@
  */
 int main(void)
 {
-int c = 0;
-int f_d;
-int l_d;
-int c2;
-int f_d2;
-int l_d2;
-while (c <= 98)
+int a, b;
+for (a = 0; a < 99; a++)
 {
-f_d2 = (c2 / 10 + '0');
-l_d2 = (c2 % 10 + '0');
-c2 = 0;
-while (c2 <= 99)
+for (b = a + 1; b < 100; 100; b++)
 {
-f_d2 = (c2 / 10 + '0');
-l_d = (c2 % 10 + '0');
-if (c < c2)
-{
-putchar(f_d);
-putchar(l_d);
+putchar((a / 10) + '0');
+putchar((a % 10) + '0');
 putchar(' ');
-putchar(f_d2);
-putchar(l_d2);
-if (c != 98)
-{
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+if (a == 98 && b == 99)
+continue;
 putchar(',');
-putchar(' ');
+putchar(',');
 }
 }
-c2++;
-}
-c++;
-}
-putchar('\n');
+iiputchar('\n');
 return (0);
 }
-
