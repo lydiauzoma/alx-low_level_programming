adb97c0bd6f73c20c81c7d590d01d7705a73d5c0
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-04 07:12:41 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int a, b;
-for (a = 0; a < 99; a++)
+for (a = 0; a <=  98; a++)
 {
-for (b = a + 1; b < 100; b++)
+for (b = a + 1; b <=  99; b++)
 {
 putchar((a / 10) + '0');
 putchar((a % 10) + '0');
