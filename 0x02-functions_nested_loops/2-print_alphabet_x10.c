413523056bf55b06356bc1b571cbf4775325bef0
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 12:12:43 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - make alphabete ten times
+ *
+ * Return:alway 0
+ *
+ */
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 'a'; j <= 'z' j++)
+	{
+	_putchar(j);
+	}
+	putchar('\n');
+	}
+	}
+ 
