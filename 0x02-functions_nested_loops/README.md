b3c783e5e77f267ddc583fa8837a39f7a6a31aac
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 09:59:15 -0700

    task 0

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# C - Functions, nested loops
+.0. _putchar
+program that prints _putchar, followed by a new line.
+
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - calls other functions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printsomething();
+	return (0);
+}
