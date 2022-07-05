8ae5b6a8db6ec366ac952dcd7b486b03f3a09623
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 13:27:06 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  * Return:alway 0
  *
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int j;
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (j = 'a'; j <= 'z' j++)
+	for (j = 'a'; j <= 'z' j++)
 	{
 	_putchar(j);
 	}
