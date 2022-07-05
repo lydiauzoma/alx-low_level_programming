e80e00930f698809db42883733133082bb0bf174
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 11:35:48 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the lowercase alphabets
+ *Return 0
+ */
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+{
+	_putchar(j);
+}
+	_putchar('/n');
+}
