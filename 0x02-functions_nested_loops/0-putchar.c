947b35eb5bbb7f84d25eeef630396b982ae4988a
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 10:09:56 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* printsomething - prints "_putchar"
-* Return: nothing
+* main - prints "_putchar"
+* Return: 0
 */
-void printsomething(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -14,5 +14,5 @@ void printsomething(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
+	return (0);
 }
