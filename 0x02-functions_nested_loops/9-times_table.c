dd3a50580111831abcc47597e982761049fcf16d
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 19:47:56 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,23 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; 1 < 10; i++)
+	for (i = 0; 1 < 9; i++)
 	{
-	for (j = 0; j < 10; j++)
+	for (j = 0; j < 9; j++)
 	{
 	k = j * i;
 	if (j == 0)
 	{
 	_putchar(k + '0');
 	}
-	if (k < 10 && j != 0)
+	if (k < 9 && j != 0)
 	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
 	}
-	else if (k >= 10)
+	else if (k >= 9)
 	{
 	_putchar(',');
 	_putchar(' ');
@@ -31,7 +31,7 @@ void times_table(void)
 	_putchar((k % 10) + '0');
 	}
 	}
-	_putchar('\n');
+_putchar('\n');
 	}
 }
 
