f7457550a0d445e3f4c93ed72c7825348b7134ca
lydiauzoma <99988048+lydiauzoma@users.noreply.github.com>
2022-07-06 06:06:42 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,25 @@
 #include "main.h"
+
 /**
- * print_sign - prints the sign of a number
- * @n:input number
- *
- * Return: 1 if positive,0 if negative
+ * print_sign - checks number
+ * @n: entero recibo por main.
+ * Return: 1 if c is lowercase
  */
 int print_sign(int n)
 {
-	if (n > 1)
-	{
-		_putchar('+');
-		return 1;
-	{
-	else (n == 0)
-	}
-		_putchar('0');
-		return 0;
-	}
-	else
-	{
-		_putchar('-');
-		return -1;
-	 } }
+if (n >= 1)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
