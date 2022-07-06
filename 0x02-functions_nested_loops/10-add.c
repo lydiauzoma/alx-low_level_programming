29f09c5a7b3963acd0977f18db8ed387d45e7c2c
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 18:39:25 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Check main
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that add two integers.
+ * Return:Result of addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ void print_alphabet_x10(void);
  int print_last_digit(int);
  void jack_bauer(void);
  void times_table(void);
+ int add(int, int);
 	
 #endif
