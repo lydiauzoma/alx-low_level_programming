d9b8c1d1c22b511c9f4de97d5afb3d410b667b10
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-06 08:55:59 -0700

    task 14

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of rhe even-valued term
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%lu", sum);
+	return (0);
+}
