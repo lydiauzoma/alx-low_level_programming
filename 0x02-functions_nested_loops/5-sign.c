64294c9cefe90f0af89699cf0b09ad67afe0cdcc
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-05 20:13:06 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n >= 1)
 
 	{
 		_putchar('+');
@@ -15,7 +15,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return ('0');
 	}
 	else
