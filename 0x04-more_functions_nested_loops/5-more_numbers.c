dfd8bd00c7afb5884fd12dea96d46a7c0f979f3c
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 14:53:22 -0700

    task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,10 @@ void more_numbers(void)
 	{
 	for (y = 0; y < 15; y++)
 	{
-	_putchar(y + '0');
+	if (y >= 10)
+		_putchar((y /10) + '0')
+	else
+		_putchar(y + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 	}
-}
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * print_line -> printing line
- * @n: integer params
+ * @n: integer _to be printed
  * Return: success
  */
+
 void print_line(int n)
 {
-	int x;
+	int i = 0;
 
-	if (n <= 0)
-	_putchar('/n');
-	for (x = 0; x <= n; x++)
-	_putchar('_');
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		1++
+	}
 	_putchar('\n');
 }
