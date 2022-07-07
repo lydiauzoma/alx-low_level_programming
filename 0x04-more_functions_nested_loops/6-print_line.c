e2e807f81fea8b2b4b6113bc5de13b3814acf607
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 14:58:05 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * print_line -> printing line
  * @n: integer _to be printed
- * Return: success
+ * Return: void
  */
 
 void print_line(int n)
@@ -12,7 +13,7 @@ void print_line(int n)
 	while (i < n && n > 0)
 	{
 		_putchar('_');
-		1++
+		1++;
 	}
 	_putchar('\n');
 }
