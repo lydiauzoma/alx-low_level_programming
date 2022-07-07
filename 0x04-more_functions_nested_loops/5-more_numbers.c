5281a193dc159c680037738d6bfae80f2985028d
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 13:26:33 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers -> printing more numbers
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; < 10; x++)
+	{
+	  for (y = 0; y < 15; y++)
+	  {
+		  _putchar(y + '0');
+	}
+}
