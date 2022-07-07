3a7355950520228474c42ab52f03c2ad54b4ae25
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 15:16:15 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check for a number
+ * @n: number of \\ to be printed
+ * Return:void
+ */
+void print_square(int size)
+{
+	int i = 0, ii;
+
+	while (i < && n > 0)
+	{
+		ii = 0;
+		while (ii < i)
+	{
+		_putchar(' ');
+		ii++;
+	{
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if ( == 0)
+		_putchar('\n');
+}
