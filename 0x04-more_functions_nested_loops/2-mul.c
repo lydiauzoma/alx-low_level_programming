43c1be842a7cff9b1ccb3c86461b2b86098fb35e
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 11:32:37 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -> Multiple  2 numbers
+ * @a: interger num 1
+ * @b: interger num 2
+ * Return: success and interger
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
