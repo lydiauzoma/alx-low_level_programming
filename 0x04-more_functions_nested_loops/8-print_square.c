257810089eff78a24fd1d41e5db86f647d615245
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 16:09:55 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,19 +7,18 @@
  */
 void print_square(int size)
 {
-	int x;
+	int x, y;
+	int size = 2;
 
 	if (size <= 0)
 	_putchar('\n');
 	else
 	{
 	for (x = 0; x < size; x++)
-	for (y = 0; y < size: y++)
+	for (y = 0; y < size; y++)
 	{
 	_putchar(35);
 	}
 	_putchar('\n');
 	}
-	_putchar('\n');
 }
-
