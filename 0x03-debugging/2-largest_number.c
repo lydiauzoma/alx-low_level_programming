3b04520bfe9e654aff32d24531e7e5badfb85ce6
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 08:30:03 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= a && a >= c)
+	if (a >= b && a >= c)
 	{
 	largest = a;
 	}
@@ -26,4 +26,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
