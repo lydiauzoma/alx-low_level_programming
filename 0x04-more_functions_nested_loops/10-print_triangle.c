55893114e607d61e08f8d26425c09fa0a58da37d
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 17:37:39 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for numbers
+ * @size: integer type
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, ii;
+
+	while (i <= size  && size > 0)
+	{
+	ii = 0;
+	while (ii < size - i)
+	{
+	_putchar(' ');
+	ii++;
+	}
+	ii = 0;
+	while (ii < i)
+	{
+	_putchar('#');
+	ii++
+	}
+	_putchar('\n');
+	i++;
+	}
+	if (i == 1)
+	_putchar('\n');
+}
