a177e535616a540b6e2d8a659ce5e5d911f74ed8
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-07 18:33:29 -0700

    task 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 
 	while (i < 10)
 	{
-	for (n = 0; n, <= 14; n++)
+	for (n = 0; n  <= 14; n++)
 	{
 	c = n;
 	if (n > 9)
 	{
-	_putchar('1);
+	_putchar('1');
 	c = n % 10;
 	}
 	_putchar('0' + c);
