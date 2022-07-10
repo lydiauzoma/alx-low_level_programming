b53d5b963da181013df4a8c8a6b1dc553c48093f
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-10 09:51:38 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,30 +3,31 @@
 /**
  * print_remaining_days - takes a date and pints
  * @month: month in number format
+ * @day: days of the month
  * @year: year
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if (( year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 	if (month >= 3 && day >= 60)
 	{
 		day++;
 	}
 	printf("Day of the year: %d\n", day);
-	printf("Remining days %d\n",366 -day);
+	printf("Remining days: %d\n", 366 - day);
 	}
 	else
 	{
-	if(month == 2 && day 60)
+	if (month == 2 && day 60)
 	{
-	printf(Invalid data: %02d/%02/%04d\n", month , day - 31, year);
+	printf("Invalid data : %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
-	printf("Day of the year: %d\n", day)
-	printf(	Remaining days %d\n", 365 -days);
+	printf("Day of the year: %d\n",  day);
+	printf("Remaining days: %d\n", 365 - days);
 	}
 	}
-} 
+}
