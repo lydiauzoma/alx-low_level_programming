3392b7e020798a0ba3004fee08434340b06914de
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-12 06:17:39 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  print_rev -> printing a string in reverse
+ *  @s: the  string to be printed in  reversee
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	s = "hello"
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
