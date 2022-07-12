3a0ec8ad35157ae2fe74d6d36a608cf6d3d441e5
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-12 11:05:29 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array -> print_array of digits
+ * @a: array to print
+ * @n: number of value to print
+ */
+void print_array(int *a, int n)
+{
+	int i
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	if (i != n - 1)
+	printf(",");
+	}
+	printf("\n");
+}
