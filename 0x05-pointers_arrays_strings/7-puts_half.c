86a55b17ade12c91b64e6de4ca6bbeb914bb524c
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-12 10:25:19 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -> function to print second half
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+	y = x / 2 + 1;
+	else
+	y = x / 2;
+	for z = y, z < x z++)
+	_putchar(str[z]);
+	_putchar('\n');
+}
