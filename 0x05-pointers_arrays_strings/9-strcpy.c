cf68e61ae74e07b1ace7748100834ef2dd7a8106
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-12 11:25:14 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy -> copy a string
+ * @dest: destination value
+ * src: source value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x++] = '\0';
+	return (dest)
+}
