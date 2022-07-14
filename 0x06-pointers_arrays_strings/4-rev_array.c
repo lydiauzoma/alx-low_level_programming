e7f7c52b9688c634e6347e0c8007d1dd851d78c8
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-14 05:00:11 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array -> function reversing an array
+ * @a: array a
+ * @n: an element of  an array
+ */
+void reverse_array(int *a, int n)
+{
+	int *x, i, aux, k;
+
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *x;
+		*x = aux;
+		p--;
+	}
+}
