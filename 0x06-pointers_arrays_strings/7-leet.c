bc6c21e3bcd0f827dd74b5059ac0dd7b96a50b16
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-14 07:37:17 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,14 +6,14 @@
  */
 char *leet(char *x)
 {
-	int a = 0, b, 1 = 5;
-	char tr[5] = {'A', 'E', 'O', 'T' 'L'};
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
 	while (x[a])
 	{
 	b = 0;
-	while (b < 1)
+	while (b < l)
 	{
 	if (x[a] == tr[b] || x[a] - 32 == tr[b])
 	x[a] = trw[b];
