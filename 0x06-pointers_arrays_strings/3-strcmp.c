e4fc5215572d3970a3875497b4a45f20f58ba21d
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-14 04:42:19 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -> function to compare
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
