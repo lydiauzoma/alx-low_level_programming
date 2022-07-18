678ca3426b5eadf64ecfbf37b10d8c9e3e2e6245
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-18 09:35:15 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*set_string-sets the value of the pointer
+*@s:parameter to modify
+*@to:parameter to assign
+*Return: nothing
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
