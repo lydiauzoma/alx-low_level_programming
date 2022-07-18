3f2a4bbaab49c8c0369690d2f8623fa8a9f51c60
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-18 07:32:35 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn -> search a string for a set of bytes
+ * @s: source string
+ * @accept: string accepted
+ * Return: number of bytes in the segement
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0, y, z = 0;
+
+	while (accept[x])
+	{
+		y = 0;
+
+		while (s[y] != 32)
+		{
+			if (accept[x] == s[y])
+			{
+				z++;
+			}
+
+			y++;
+		}
+		x++
+	}
+	return (z);
+}
+
+
