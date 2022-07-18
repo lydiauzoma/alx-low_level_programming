86c17afef3bbc3407c71add2be65c7c43892dc16
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-18 05:31:46 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
-*_memset -> function that file memory
+*_memset: function that file memory
 *@s: a  string
 *@b: a character
 *@n: an  integer
@@ -14,3 +14,4 @@ char *_memset(char *s, char b, unsigned int n)
 	for (x = 0; x < n; x++)
 		s[x] = b;
 	return (s);
+}
