79dfcdde85438e65eae7bfcb17959f3f11c14a28
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-18 06:56:35 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr -> this  string to be scanned
+ * @s: the character to be searched in str
+ * @c: another character to be searched in str
+ * Return: a string
+ */
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	while (1)
+	{
+		x = *s++
+		if (x == c)
+		{
+			return (s - 1);
+		}
+		if (x == 0)
+		{
+			return (NULL);
+		}
+	}
+}
