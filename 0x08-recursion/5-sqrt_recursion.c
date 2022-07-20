c1c593b5e8a610d58464810867b01567706de157
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-20 15:23:21 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,12 +12,12 @@ int _sqrt_recursion(int n)
 	return (_sqrt(n, 1));
 }
 /**
-*_sqrt_recursion->recursive squar root
+*_sqrt->recursive squar root
 *@n: number parameter
 *@i: iterator
 *Return: a number
 */
-int _sqrt_recursion(int n, int i)
+int _sqrt(int n, int i)
 {
 	int square = i * i;
 
