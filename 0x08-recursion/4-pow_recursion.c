e179db098a1fd0075eaeb1626839048e58a154c0
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-20 10:57:29 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion -> function to return x rised to the power of y
+ * @x: parameter x^y
+ * @y: power result
+ * Return: return if success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1)
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
