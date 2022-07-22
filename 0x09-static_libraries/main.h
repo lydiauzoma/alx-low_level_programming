909ddc17e7eb3fb49ed283c97fe7d795c778fe31
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-22 09:13:01 -0700

    task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -8,7 +8,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
