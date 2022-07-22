558283c0f704ee1901f5ea94cc98a205d4c5d94e
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-22 11:43:06 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+ *main ->  function  multiplies two 2 numbers
+ *@argc:argc parameter
+ *@argv: an array
+ *Return: 1 if not passed and 0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int result, x, y;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+
+	printf("%d\n", result);
+	return (0);
+}
