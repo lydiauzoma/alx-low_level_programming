8a7262fd15a01c844d3e58d9f1bf7c137aa3728a
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-28 11:29:10 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
 
-	for (index = 0; s2[index] && index < n, index++)
+	for (index = 0; s2[index] && index < n; index++)
 		concat[len++] = s2[index];
 	concat[len] = '\0';
 	return (concat);
