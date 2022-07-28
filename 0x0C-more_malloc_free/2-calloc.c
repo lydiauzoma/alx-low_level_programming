5cd7a6d6eb27f2fe47d90fef9e78322869f809fe
lydiauzoma <lydiauzoma@yahoo.com>
2022-07-28 10:25:16 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc -> function allocates memory for array
+ *@nmemb:The number of element 
+ *@size:The byte size of each array
+ *Return: if nmemb = 0 , size = 0 or function fails = NULL.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	void *non;
+	char *scan;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+
+		if (non == NULL)
+			return (NULL);
+	scan = non;
+
+	for (index = 0; index < (size * nmemb); index++)
+		scan[index] = '\0';
+	return (non);
+}
