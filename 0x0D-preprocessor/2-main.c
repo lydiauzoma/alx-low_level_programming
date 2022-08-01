ee16aecad4be2ebcb4aeef34eed319793f359b0b
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-01 11:23:32 -0700

    all commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main -> print the name of the file
+ * Return:Always 0.
+ */
+int main(void)
+{
+	print("%s\n", _FILE_);
+	return(0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef SUM_A
+#define SUM_A
+#define SUM(x, y) ((x) + (y))
+#endif
