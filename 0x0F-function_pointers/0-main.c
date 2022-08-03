082b58db648efbdcb44e574256dd04fae03246ae
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-03 13:22:36 -0700

    task 0

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -20,6 +20,7 @@ void print_name_as_is(char *name)
 void print_name_uppercase(char *name)
 {
 	unsigned int i;
+
 	printf("Hello, my uppercase name is ");
 	i = 0;
 	while (name[i])
--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,8 @@
 #include <stdlib.h>
-
 /**
-<<<<<<< HEAD
  * print_name - prints a name.
  * @name: input name.
- * @f: function to execute
-=======
- * print_name - execute a function given as a parameter
- * @name: parameter that function ptr needs
- * @f:function to execute
->>>>>>> 290646aba3b8571facd449cac80003a4b4017f5d
+ * @f: function to execut
  */
 
 void print_name(char *name, void (*f)(char *))
