0afbba87fd09a07720d7277b64c0f0b4d5a1bca2
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-03 08:02:29 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *array_iterator -> excute on an array of element
+ *@size: size of the array
+ *@array: input integer array
+ *@action: pointer to the function
+ *Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	if (array !=  NULL && action != NULL && size != 0)
+{
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+}
