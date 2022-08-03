49502133d89625a97b7206b8750e104356fb6e41
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-03 08:07:03 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array !=  NULL && action != NULL && size != 0)
 {
-	for (x = 0; i < size; x++)
+	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
 	}
