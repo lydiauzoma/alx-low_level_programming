80e3d2d1912a638a223363ff659f16a4cb08c666
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-15 12:59:15 -0700

    correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,12 +7,14 @@
  *
  * Return: the number of element in the listint_t list
  */
- size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
+	size_t nodes = 0;
+
 	while (h)
 	{
-		nodes++
+		nodes++;
 		h = h->next;
 	}
-	return(nodes);
+	return (nodes);
 }
