cfc9e994f339407afafdced432198b9f4903b1e7
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-15 13:11:51 -0700

    hot fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		else
 			return (NULL);
-		if (console != NULL);
+		if (console != NULL)
 		{
 			while (console->next != NULL)
 				console = console->next;
