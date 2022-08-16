e36af71b50f83e93b83c250cbcdd0c2310beb500
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-16 03:33:45 -0700

    hot fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
 	size_t count = 0;
 
-	while (cursor != NULL)
+	while (h)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (count);
 }
