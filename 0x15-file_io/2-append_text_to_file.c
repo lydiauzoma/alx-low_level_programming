016d0db9ad0930ff7751ac0c57858439affa8750
lydiauzoma <lydiauzoma@yahoo.com>
2022-08-22 11:08:45 -0700

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,12 +11,12 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
-	
+
 	if (filename == NULL)
 	return (-1);
 	if (text_content != NULL)
 	{
-	for (len = 0; text_content[len];)	
+	for (len = 0; text_content[len];)
 		len++;
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
